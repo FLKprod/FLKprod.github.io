480cef22aa166afe4e48b0e4e665b47753b84719
Maxou <105202869+FLKprod@users.noreply.github.com>
2024-03-17 22:16:02 -0400

    Delete docs directory
    Suppresion ancienne version

--- docs/BoulderDash/js/regles.js
+++
@@ -1,8 +0,0 @@
-//-------------------------- BOUTON DE RETOUR AU MENU -------------------------------------------//
-
-const retour = document.getElementById("Retourmenu")
-retour.addEventListener("click",()=>{
-    window.location.href='./../html/index.html';
-});
-
-//-----------------------------------------------------------------------------------------------//
