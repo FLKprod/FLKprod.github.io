7ff59fd9899768a4362fd4d5571efd89e04d8043
Maxou <105202869+FLKprod@users.noreply.github.com>
2024-03-19 10:58:28 -0400

    update structure

---
+++ js/createElements.js
@@ -0,0 +1,173 @@
+export function createTextElement(tag, textContent) {
+    const element = document.createElement(tag);
+    element.textContent = textContent;
+    return element;
+}
+
+export function createImage(src) {
+    const element = document.createElement('img');
+    element.src = src;
+    return element;
+}
+
+export function createText(tag, textContent) {
+    return createTextElement(tag, textContent);
+}
+
+export function createIcon(iconClass, fontSize, color, cursor, clickHandler) {
+    const icon = document.createElement("i");
+    icon.className = `fa ${iconClass}`;
+    icon.style.fontSize = fontSize;
+    icon.style.color = color;
+    icon.style.cursor = cursor;
+    icon.onclick = clickHandler;
+    return icon;
+}
+
+export function createButton(text, clickHandler) {
+    const button = document.createElement("button");
+    button.textContent = text;
+    button.onclick = clickHandler;
+    return button;
+}
+
+export function createInfoSection(data) {
+    const infoDiv = document.createElement("div");
+    infoDiv.className = "info";
+
+    data.forEach((item, index) => {
+        if (item.type === 'paragraph') {
+            const paragraph = document.createElement("p");
+            paragraph.textContent = item.text;
+            if (index === 1) {
+                paragraph.classList.add('bold-text');
+            }
+            infoDiv.appendChild(paragraph);
+        } else if (item.type === 'icon') {
+            const icon = document.createElement("i");
+            icon.className = item.iconClass;
+            icon.style.fontSize = item.fontSize;
+            icon.style.color = item.color;
+            icon.style.cursor = item.cursor;
+            icon.onclick = item.clickHandler;
+            infoDiv.appendChild(icon);
+        } else if (item.type === 'button') {
+            const button = createButton(item.text, item.clickHandler);
+            infoDiv.appendChild(button);
+        }
+    });
+    return infoDiv;
+}
+
+export function createSocialLink(href, iconClass) {
+    const link = document.createElement("a");
+    link.href = href;
+    link.target = "_blank";
+    const icon = document.createElement("i");
+    icon.classList.add("fa", iconClass);
+    link.appendChild(icon);
+    return link;
+}
+
+export function createElementWithClass(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+export function createInput(type, id, placeholder, style) {
+    const input = document.createElement('input');
+    input.type = type;
+    input.id = id;
+    input.placeholder = placeholder;
+    input.style.cssText = style;
+    return input;
+}
+
+export function createLabel(text, forId) {
+    const label = document.createElement('label');
+    label.textContent = text;
+    label.htmlFor = forId;
+    return label;
+}
+
+export function createTableContainer() {
+    const tableContainer = createElementWithClass('div', 'tableContainer');
+    const table = createElementWithClass('div','vulnerabilityTable');
+    const vulnerabilityTable = document.createElement('table');
+    vulnerabilityTable.id = 'vulnerabilityTable';
+    menu = createMenu();
+    tableContainer.appendChild(menu);
+    table.appendChild(vulnerabilityTable);
+    tableContainer.appendChild(table)
+    return tableContainer;
+}
+
+export function createSelectElement(id, onchange) {
+    const select = document.createElement('select');
+    select.id = id;
+    select.onchange = onchange;
+    return select;
+}
+
+export function createOption(value, text, disabled, selected) {
+    const option = document.createElement('option');
+    option.value = value;
+    option.textContent = text;
+    option.disabled = disabled;
+    option.selected = selected;
+    return option;
+}
+
+export function createVideo(link) {
+    const iframeElement = document.createElement('iframe');
+    iframeElement.src = link;
+    iframeElement.title = "YouTube video player";
+    iframeElement.frameborder = "0";
+    iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframeElement.allowfullscreen = true;
+    return iframeElement;
+}
+
+export function createOverlayImage(src) {
+    return createImageElement('img', 'overlay-image', src, 'Image de coin bas droit');
+}
+
+export function createImageElement(tag, className, src, alt) {
+    const element = document.createElement(tag);
+    element.className = className;
+    element.src = src;
+    element.alt = alt;
+    return element;
+}
+
+export function createVideoWithOverlay(link, imageSrc) {
+    const videoContainer = createElementWithClass('div', 'video-projet');
+    const videoElement = createVideo(link);
+    const overlayImage = createOverlayImage(imageSrc);
+
+    videoContainer.appendChild(videoElement);
+    videoContainer.appendChild(overlayImage);
+
+    return videoContainer;
+}
+
+
+export function createVideoElement(videoUrl, description) {
+    var videoDiv = createElementWithClass("div", "video");
+    var videoElement = createVideo(videoUrl);
+    videoDiv.appendChild(videoElement);
+    videoDiv.appendChild(createText('p', description));
+    return videoDiv;
+}
+
+
+/************************** UPDATES  ******************************************/
+export function updateVideoElement(videoUrl) {
+    var videoElement = document.querySelector('iframe');
+    if (videoElement){
+        console.log('video trouvee' + videoElement.src);
+    }
+    videoElement.src = videoUrl;
+
+}
---
+++ js/index.js
@@ -0,0 +1,208 @@
+import { createTextElement, createImage, createText, createSocialLink, createInfoSection, createButton, createIcon, createElementWithClass, createInput, createLabel, createTableContainer, createSelectElement, createOption, createVideo, createOverlayImage, createImageElement, createVideoWithOverlay, createVideoElement, updateVideoElement } from './createElements.js';
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+toggleTeamInfo('projets');
+var introGif = document.getElementById("introGif");
+var mainContent = document.getElementById("mainContent");
+setTimeout(function() {
+    introGif.style.opacity = 0;
+    introGif.removeAttribute("loop");
+    mainContent.style.opacity = 1;
+    mainContent.classList.remove('hidden');
+}, 0);
+
+/******************************* MODE NUIT  *****************************************************************/
+
+const daynightCheckbox = document.getElementById('daynight');
+const logo = document.getElementById('logo');
+function toggleModeNuit() {
+    if (daynightCheckbox.checked) {
+        document.body.classList.add('mode-nuit');
+        logo.src = "Photos/logo.png";
+    } else {
+        document.body.classList.remove('mode-nuit');
+        logo.src = "Photos/logoneg.png";
+    }
+}
+
+toggleModeNuit();
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function() {
+    daynightCheckbox.checked = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    toggleModeNuit();
+});
+
+document.getElementById('daynight').addEventListener('change', toggleModeNuit);
+
+const buttonIds = ['presentation', 'projets', 'videos', 'contact'];
+buttonIds.forEach(buttonId => {
+    const button = document.getElementById(buttonId);
+    button.addEventListener('click', function() {
+        toggleTeamInfo(buttonId);
+    });
+});
+
+function toggleTeamInfo(id) {
+    var buttons = document.querySelectorAll('.menu button');
+    buttons.forEach(function(button) {
+        button.classList.remove('select');
+    });
+    var selectedButton = document.getElementById(id);
+    if (selectedButton) {
+        console.log(id);
+        selectedButton.classList.add('select');
+    }
+
+    var presentationContainer = document.querySelector('.presentation-container');
+    var videosContainer = document.querySelector('.videos-container');
+    var projetsContainer = document.querySelector('.projets-container');
+    var contactContainer = document.querySelector('.contact-container');
+    presentationContainer.innerHTML = '';
+    videosContainer.innerHTML = '';
+    projetsContainer.innerHTML = '';
+    contactContainer.innerHTML = '';
+    videosContainer.classList.remove('fade-in');
+    projetsContainer.classList.remove('fade-in');
+    contactContainer.classList.remove('fade-in');
+    presentationContainer.classList.remove('fade-in');
+    if(id=== 'videos'){
+        videosContainer.classList.add('fade-in');
+        videosContainer.appendChild(createText('h2',"Mes contenus vidéos"));
+        videosContainer.appendChild(createText('p',"Passionné par le montage vidéo et constamment à la recherche d'inspiration pour créer du contenu, je saute sur l'occasion de concrétiser une idée germeant dans mon esprit, à travers des vidéos captivantes et originales. Vous pouvez consulter toutes mes creations dans la rubrique ci-dessous. Enjoy."));
+        
+        var galerie =createElementWithClass("div","galerie-videos");
+        galerie.appendChild(createImageElement("img", 'pelicule', "Photos/pelicule.jpg", "Description de l'image"));
+
+        var videos = createElementWithClass("div","videos");
+        videos.appendChild(createVideoElement('https://www.youtube.com/embed/gydycy1WcT0', "Vidéo introductive pour une idée de court-métrage intitulé C.Y.B.E.R (Conscience Ybridée Brouillée, Exploration Radicale), explorant la conscience numérique et l'univers du hacking."));
+        galerie.appendChild(videos);
+
+        galerie.appendChild(createImageElement("img", 'pelicule', "Photos/pelicule.jpg", "Description de l'image"));
+        videosContainer.appendChild(galerie);
+        }
+    else if(id=== 'presentation'){
+        presentationContainer.classList.add('fade-in');
+        presentationContainer.appendChild(createText('h2', "Moi, c'est Maxime"));
+        presentationContainer.appendChild(createText('p',"Bonjour, je suis Maxime Falkowski, un étudiant en maîtrise informatique spécialisé en cybersécurité à l'Université du Québec à Chicoutimi (UQAC). En parallèle, je suis également étudiant à l'École Supérieure d'Ingénieurs de Recherche en Matériaux (ESIREM), où je me concentre sur l'ingénierie de la sécurité et la qualité des réseaux."));
+        presentationContainer.appendChild(createText('p',"Ma passion pour la sécurité informatique se conjugue avec mon intérêt pour la photographie et la vidéo. En dehors des salles de cours, je m'investis pleinement dans la création de contenus visuels que je partage sur mes réseaux sociaux. Vous pouvez explorer mes réalisations artistiques sur mon site internet, où je présente mes projets photographiques et vidéographiques."));
+        presentationContainer.appendChild(createText('h3', 'Mon parcours'));
+        presentationContainer.appendChild(createText('p', "Si je devais résumer ma vie et mon parcours en une phrase, ce serait la suivante."));
+        var citation = document.createElement('blockquote');
+        citation.className='citation';
+        citation.appendChild(createText('p', "Grandir sous le lys, partir chercher son or pour s'apaiser sous l'érable."));
+        presentationContainer.appendChild(citation);
+
+        presentationContainer.appendChild(createText('h4', 'Grandir sous le Lys'));
+        presentationContainer.appendChild(createText('p',"Originaire de la charmante ville de Dijon, j'y ai vécu jusqu'à mes 21 ans. Mon enfance s'est épanouie au cœur d'un quartier empreint de souvenirs, où chaque rue portait le récit de ma croissance. Les ruelles familiales ont été le théâtre de jeux insouciants, et les parcs, le refuge de découvertes infinies. Au fil des années, Dijon a été le témoin de mon évolution, de mes premiers pas à l'école jusqu'à ma maturité à l'aube de mes études supérieures. C'est dans cette cité chaleureuse que j'ai forgé mes premières aspirations et tissé des liens indéfectibles. Ma ville natale, imprégnée d'histoire et de charme, demeure le point d'ancrage de qui je suis, et ses rues résonnent encore des échos joyeux de mon passé."));
+        var imageSources = ["Photos/Dijon.jpg", "Photos/Dijon2.JPG", "Photos/Dijon3.JPG", "Photos/Dijon4.JPG"];
+        imageSources.forEach(src => {
+            const imgElement = createImage(src);
+            presentationContainer.appendChild(imgElement);
+        });
+        presentationContainer.appendChild(createText('h4',"Partir sur les terres des chercheurs d'Or" ));
+
+        presentationContainer.appendChild(createText('p',"Au cours de mes études, j'ai eu l'opportunité de réaliser un stage à Berkeley aux Lawrence Berkeley National Laboratory (LBNL), travaillant sur le projet FUEGO (Fire Urgency Estimator in Geostationary Orbit). Cette expérience captivante a été bien plus qu'une simple opportunité professionnelle ; elle a été la concrétisation de mon rêve américain. Travailler au cœur de l'innovation technologique à Berkeley m'a offert une expérience unique, me permettant de contribuer au développement d'un outil de surveillance avancé pour estimer l'urgence des incendies. FUEGO exploite des technologies de pointe en télédétection pour évaluer en temps réel l'intensité et la propagation des incendies, combinant ces données avec des informations météorologiques et des modèles prédictifs. L'objectif est d'estimer rapidement la gravité des incendies, facilitant ainsi une réponse plus rapide et plus efficace des équipes d'intervention."));
+        var imageSources = ["Photos/SF.JPG", "Photos/SF2.JPG", "Photos/SF3.JPG", "Photos/SF4.JPG"];
+        imageSources.forEach(src => {
+            const imgElement = createImage(src);
+            presentationContainer.appendChild(imgElement);
+        });
+        presentationContainer.appendChild(createText('h4',"Se reposer sous l'Erable" ));
+
+        presentationContainer.appendChild(createText('p',"Par la suite, j'ai choisi d'effectuer un parcours de double diplôme à Chicoutimi, à l'Université du Québec, et ma découverte du Canada n'a fait que renforcer mon appréciation pour ce pays. Le Canada m'a accueilli chaleureusement, offrant une qualité de vie exceptionnelle et une diversité culturelle qui a enrichi mon expérience académique. Cette expérience au Canada, combinée à mes voyages et expériences antérieurs, m'ont incontestablement ouvert l'esprit sur la diversité des approches et des perspectives en matière de technologie et de sécurité informatique. Voyager et vivre dans des endroits aussi différents a enrichi ma vision du monde, me dotant d'une compréhension approfondie des défis technologiques à l'échelle mondiale. Ainsi, mon parcours reflète ma volonté constante d'explorer, d'apprendre et de m'adapter aux évolutions de notre société de plus en plus interconnectée."));
+        var imageSources = ["Photos/Canada.JPG", "Photos/Canada2.JPG", "Photos/Canada3.JPG", "Photos/Canada4.JPG"];
+        imageSources.forEach(src => {
+            const imgElement = createImage(src);
+            presentationContainer.appendChild(imgElement);
+        });
+
+            
+        }
+        else if(id === 'contact'){
+            contactContainer.classList.add('fade-in');
+            const itemscontactContainer = document.createElement("div");
+            itemscontactContainer.classList.add("contact-text");
+            var infoSectionData = [
+                { type: 'icon', iconClass: "fa fa-envelope", clickHandler: function () {
+                    window.location.href = "mailto:contact.flkprod@gmail.com";
+                }},
+                { type: 'paragraph', text: "Adresse courriel" },
+                { type: 'paragraph', text: "contact.flkprod@gmail.com" }
+            ];
+            contactContainer.appendChild(createText('h2', "Me contacter ? C'est par ici !"));
+            itemscontactContainer.appendChild(createInfoSection(infoSectionData));
+
+
+            infoSectionData = [
+                { type: 'icon', iconClass: "fab fa-github", clickHandler: function () {
+                    window.open("https://github.com/FLKprod", "_blank");
+                }}, { type: 'paragraph', text: "Github" },
+                { type: 'paragraph', text: "Page officielle" }
+            ];
+            itemscontactContainer.appendChild(createInfoSection(infoSectionData));
+            
+            infoSectionData = [
+                
+                { type: 'icon', iconClass: "fab fa-instagram", clickHandler: function () {
+                    window.open("https://www.instagram.com/flkprod_/", "_blank");
+                }},{ type: 'paragraph', text: "Instagram" },
+                { type: 'paragraph', text: "Page officielle" }
+            ];
+            itemscontactContainer.appendChild(createInfoSection(infoSectionData));
+            infoSectionData = [
+                { type: 'icon', iconClass: "fab fa-facebook", clickHandler: function () {
+                    window.open("https://www.facebook.com/profile.php?id=100070487814685", "_blank");
+                }},{ type: 'paragraph', text: "Facebook" },
+                { type: 'paragraph', text: "Page officielle" }
+            ];
+            itemscontactContainer.appendChild(createInfoSection(infoSectionData));
+            infoSectionData = [
+                { type: 'icon', iconClass: "fab fa-tiktok", clickHandler: function () {
+                    window.open("https://www.tiktok.com/@flkprod", "_blank");
+                }}, { type: 'paragraph', text: "TikTok" },
+                { type: 'paragraph', text: "Page officielle" }
+            ];
+            itemscontactContainer.appendChild(createInfoSection(infoSectionData));
+            contactContainer.appendChild(itemscontactContainer);
+        }
+        else if(id === 'projets'){ 
+            projetsContainer.classList.add('fade-in');
+            projetsContainer.appendChild(createText('h2',"Mes contenus vidéos"));
+            projetsContainer.appendChild(createText('p',"Passionné par le montage vidéo et constamment à la recherche d'inspiration pour créer du contenu, je saute sur l'occasion de concrétiser une idée germeant dans mon esprit, à travers des vidéos captivantes et originales. Vous pouvez consulter toutes mes creations dans la rubrique ci-dessous. Enjoy."));
+            
+            var projets = createElementWithClass('div','projets');
+            
+            var menuDiv = createElementWithClass('div','menu3');
+            var menuList = [
+                { name: "Flkprod.github.io", videoLink: "chemin_video_1.mp4", imageSrc: "chemin_image_1.jpg" },
+                { name: "BoulderDash", videoLink: "chemin_video_2.mp4", imageSrc: "chemin_image_2.jpg" },
+                { name: "94 Degrés", videoLink: "chemin_video_3.mp4", imageSrc: "chemin_image_3.jpg" },
+                { name: "TowerDefender", videoLink: "chemin_video_4.mp4", imageSrc: "chemin_image_4.jpg" },
+                { name: "ToDoList pour IOs", videoLink: "chemin_video_5.mp4", imageSrc: "chemin_image_5.jpg" },
+                { name: "Carte intéractive", videoLink: "chemin_video_6.mp4", imageSrc: "chemin_image_6.jpg" },
+                { name: "CyberSafe", videoLink: "chemin_video_7.mp4", imageSrc: "chemin_image_7.jpg" },
+                { name: "LanbdaCash", videoLink: "Photos/1216.mp4", imageSrc: "Photos/SF.jpg" }
+            ];
+            for (let i = 0; i < menuList.length; i++) {
+                menuDiv.appendChild(createTextElement('ul',menuList[i].name));
+            }
+            projets.appendChild(menuDiv);
+
+            var video=createVideoWithOverlay('','');
+            projets.appendChild(createVideoWithOverlay('',''));
+            projets.appendChild(createTextElement('h2', ''));
+            projets.appendChild(createTextElement('p', 'Description du projet...'));
+
+            menuDiv.addEventListener('click', function(event) {
+                if (event.target.tagName === 'UL') {
+                    
+                    var videoLink = menuList.find(item => item.name === event.target.textContent)?.videoLink;
+                    console.log("Changement de projet" + videoLink);
+                    if (videoLink) {
+                        updateVideoElement(videoLink);
+                    }
+                }});
+
+            projetsContainer.appendChild(projets);
+        }
+}
