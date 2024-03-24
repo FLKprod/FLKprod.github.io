8637c6bbbc1252513bed5c96cce2a7e4383a58c9
Maxou <105202869+FLKprod@users.noreply.github.com>
2024-03-24 11:32:45 -0400

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,7 +1,7 @@
 var scriptTag = document.createElement('script');
 scriptTag.async = true;
 scriptTag.src = 'https://www.googletagmanager.com/gtag/js?id=G-J6WB8D1GL7';
-document.appendChild(scriptTag);
+document.body.appendChild(scriptTag);
 
 var domaine = location.hostname;
 console.log(location.hostname)
