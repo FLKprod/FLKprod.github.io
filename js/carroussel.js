059cbb12e2ee6c47df35fb718f92618e282849f4
Maxou <105202869+FLKprod@users.noreply.github.com>
2024-04-06 13:20:48 -0400

    rectif bugs dans carroussels et orthograohe

--- js/carroussel.js
+++ js/carroussel.js
@@ -23,34 +23,32 @@ export function createCarousel(category, images) {
     // Fonction pour afficher l'image suivante
     function nextImage() {
       currentIndex = (currentIndex + 1) % images.length;
+      imageElement.src = images[currentIndex];
       imageElement.classList.add('fade-in');
       setTimeout(() => {
-        imageElement.src = images[currentIndex];
         imageElement.classList.remove('fade-in');
       }, 500);
-      imageElement.src = images[currentIndex];
     }
-  
+    
     // Fonction pour afficher l'image précédente
     function prevImage() {
       currentIndex = (currentIndex - 1 + images.length) % images.length;
+      imageElement.src = images[currentIndex];
       imageElement.classList.add('fade-in');
       setTimeout(() => {
-        imageElement.src = images[currentIndex];
         imageElement.classList.remove('fade-in');
       }, 500);
-      imageElement.src = images[currentIndex];
     }
+    
+    
   }
   
   // Fonction pour créer une catégorie avec son carrousel
 export function createCategoryWithCarousel(categoryName, images) {
     const categoryElement = createElementWithClass('div', 'categorie_photos');
     const titleElement = createText('h2', categoryName);
-  
     categoryElement.appendChild(titleElement);
     createCarousel(categoryElement, images);
-  
     return categoryElement;
 }
 
